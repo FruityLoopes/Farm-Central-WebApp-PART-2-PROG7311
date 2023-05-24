1705a372e0de74dbaf63d2ee877f0218fdbc378f
FruityLoopes <chadandrews07@gmail.com>
2023-05-24 16:35:50 +0200

    Add project files.

---
+++ TestingProject/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestingProject.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private static string ApiKey = "AIzaSyDhAAx7lxhP7fLRaVouLJSsxvMOzu8Ai1U";
+        
+   
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+         
+
+            return View();
+        }
+
+    }
+}
